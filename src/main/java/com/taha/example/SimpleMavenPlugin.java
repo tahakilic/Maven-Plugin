756b1d197ad6a5609f0677c06bab24561afaf21a
Taha KILIÇ <79625655+tahakilic@users.noreply.github.com>
2022-04-16 22:27:01 +0300

    Add files via upload

---
+++ src/main/java/com/taha/example/SimpleMavenPlugin.java
@@ -0,0 +1,17 @@
+package com.taha.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "version",defaultPhase = LifecyclePhase.COMPILE)
+public class SimpleMavenPlugin extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        String name="Ali";
+        String lastName="Demir";
+        getLog().info("Adınız:"+name+"\n Soyadınız:"+lastName);
+    }
+}
